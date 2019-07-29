36ea057409aa452d49a15d01a0678e6ee57484e3
Lumine <lumine.mona@outlook.com>
2019-07-29 21:31:11 +0300

    Iterator for queue and stack

--- dist/src/queue/Queue.d.ts
+++ dist/src/queue/Queue.d.ts
@@ -1,12 +1,15 @@
-export declare class Queue<T> {
+export declare class Queue<T> implements IterableIterator<T> {
     private count;
     private data;
-    constructor(data: T[]);
+    private iteratorIndex;
+    constructor(data?: T[]);
     clear(): void;
     contains(item: T): boolean;
     dequeue(): T;
     enqueue(item: T): void;
     peek(): T;
     toArray(): T[];
+    next(): IteratorResult<T>;
+    [Symbol.iterator](): IterableIterator<T>;
     readonly Count: number;
 }
