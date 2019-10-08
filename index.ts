18a7ac2e30b446de596a56de5249c9743e31c6af
Lumine <lumine.mona@outlook.com>
2019-10-08 12:37:50 +0300

    Updated exports

--- index.ts
+++ index.ts
@@ -1,9 +1,14 @@
+export { AbstractTree } from './src/tree/AbstractTree';
 export { BinarySearchTree } from './src/tree/BinarySearchTree';
 export { BinaryTree } from './src/tree/BinaryTree';
+export { ICollection } from './src/core/ICollection';
 export { IDeque } from './src/queue/IDeque';
 export { IList } from './src/list/IList';
+export { INode } from './src/tree/INode';
 export { IQueue } from './src/queue/IQueue';
 export { ISet } from './src/set/ISet';
+export { ITree, TraverseType } from './src/tree/ITree';
 export { List } from './src/list/List';
 export { Stack } from './src/stack/Stack';
+export { TreeNode } from './src/tree/TreeNode';
 export { TreeSet } from './src/set/TreeSet';
