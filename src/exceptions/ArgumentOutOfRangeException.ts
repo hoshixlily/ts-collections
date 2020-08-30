892bfe866f9d70a9df67d5bec5cb3818def76f68
Nanahoshi <nanahoshii@outlook.com>
2020-08-30 16:23:37 +0300

    Remove exception classes. Use Error instead.

--- src/exceptions/ArgumentOutOfRangeException.ts
+++
@@ -1,8 +0,0 @@
-export class ArgumentOutOfRangeException extends Error {
-    public name: string = "ArgumentOutOfRangeException";
-    public message: string = "arrayIndex is out of range.";
-    public constructor(message: string) {
-        super();
-        this.message = message;
-    }
-}
--- src/exceptions/InvalidOperationException.ts
+++
@@ -1,8 +0,0 @@
-export class InvalidOperationException extends Error {
-    name = "InvalidOperationException";
-    message = "Invalid operation."
-    public constructor(message?: string) {
-        super();
-        if (message) this.message = message;
-    }
-}
