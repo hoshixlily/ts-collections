6e27949d63f917f6a820c31af4689e4c2f63badf
Lumine <lumine.mona@outlook.com>
2020-10-14 03:19:09 +0300

    Export new classes and interfaces

--- index.ts
+++ index.ts
@@ -11,5 +11,25 @@ export {
     List,
     IQueue,
     IDeque,
-    LinkedList
+    LinkedList,
+    IDictionary,
+    KeyValuePair,
+    Dictionary,
+    ITree,
+    AbstractTree,
+    RedBlackTree,
+    ISet,
+    AbstractSet,
+    TreeSet,
+    TraverseType
 } from "./imports";
+export {Accumulator} from "./src/shared/Accumulator";
+export {EqualityComparator} from "./src/shared/EqualityComparator";
+export {IndexedAction} from "./src/shared/IndexedAction";
+export {IndexedPredicate} from "./src/shared/IndexedPredicate";
+export {IndexedSelector} from "./src/shared/IndexedSelector";
+export {JoinSelector} from "./src/shared/JoinSelector";
+export {OrderComparator} from "./src/shared/OrderComparator";
+export {Predicate} from "./src/shared/Predicate";
+export {Selector} from "./src/shared/Selector";
+export {Zipper} from "./src/shared/Zipper";
