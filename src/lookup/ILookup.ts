61ca664e9bc9c08e89d9e4c333ba34c3bc520d80
Nanahoshi <nanahoshii@outlook.com>
2020-11-30 01:47:27 +0300

    Add get method to Lookup interface

--- src/lookup/ILookup.ts
+++ src/lookup/ILookup.ts
@@ -2,6 +2,7 @@ import {IGrouping} from "../enumerator/Enumerable";
 import {IEnumerable} from "../enumerator/IEnumerable";
 
 export interface ILookup<TKey, TElement> extends IEnumerable<IGrouping<TKey, TElement>> {
+    get(key: TKey): IEnumerable<TElement>;
     hasKey(key: TKey): boolean;
     size(): number;
 }
