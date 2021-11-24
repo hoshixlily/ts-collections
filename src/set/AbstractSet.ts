43528bfd4fb099bf925f4a827a926c34b7a246a3
Lumine <lumine.mona@outlook.com>
2021-11-24 15:47:20 +0300

    Small refactor

--- src/set/AbstractSet.ts
+++ src/set/AbstractSet.ts
@@ -1,12 +1,11 @@
 import {AbstractCollection, ISet} from "../../imports";
 import {OrderComparator} from "../shared/OrderComparator";
 import {Comparators} from "../shared/Comparators";
+import {EqualityComparator} from "../shared/EqualityComparator";
 
 export abstract class AbstractSet<TElement> extends AbstractCollection<TElement> implements ISet<TElement> {
-    protected readonly orderComparator: OrderComparator<TElement>;
-    protected constructor(comparator?: OrderComparator<TElement>) {
-        super((e1: TElement, e2: TElement) => (comparator ?? Comparators.orderComparator)(e1, e2) === 0);
-        this.orderComparator = comparator ?? Comparators.orderComparator;
+    protected constructor(comparator?: EqualityComparator<TElement>) {
+        super((e1: TElement, e2: TElement) => (comparator ?? Comparators.equalityComparator)(e1, e2));
     }
 
     abstract headSet(toElement: TElement, inclusive?: boolean): ISet<TElement>;
