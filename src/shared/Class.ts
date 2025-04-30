99dfba497f43e20035ed2f051047db81f3131390
Nanahoshi <nanahoshilily@outlook.com>
2025-04-30 14:04:22 +0300

    fix: ofType return type

--- src/shared/Class.ts
+++ src/shared/Class.ts
@@ -1,1 +1,7 @@
-export type Class<T> = new(...args: unknown[]) => T;
+/**
+ * @remarks
+ * For the type of args, do not use `unknown[]` since it breaks the return type of `ofType` methods.
+ * Since I did not use this anywhere in a situation where it requires a parameter,
+ * I've opted for the ` never ` type, but in a situation where it requires a parameter, this should be changed to `any[]`.
+ */
+export type Class<T> = new(...args: never[]) => T;
